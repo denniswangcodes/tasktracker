5c8d924fdcbbc2b1968e98de315ef44a28439ee3
Dennis Wang <denniswang.self@gmail.com>
2022-10-28 23:24:09 -0700

    implemented toggleReminder and deleteTask

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
 
-export default function Task({ task }) {
+export default function Task({ task, deleteTask, toggleReminder }) {
   return (
-    <div className='task'>
-      <h3>{task.text}</h3>
+    <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => toggleReminder(task.id)}>
+      <h3>{task.text}<FaTimes style={{ color: 'red', cursor: 'pointer' }}
+        onClick={() => deleteTask(task.id)} /></h3>
       <p>{task.day}</p>
     </div>
   )
--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Task from './Task.js'
 
 
-export default function Tasks({ tasks }) {
+export default function Tasks({ tasks, deleteTask, toggleReminder }) {
 
   return (
     <>
       {tasks.map((task) => (
-        <Task task={task} />
+        <Task key={task} task={task} deleteTask={deleteTask} toggleReminder={toggleReminder} />
       ))}
     </>
   )
