a411202b3930b18ed78faa5451e94666edfddd8f
Dennis Wang <denniswang.self@gmail.com>
2022-10-30 18:16:27 -0700

    implemented react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header.js'
 import Tasks from './components/Tasks.js'
 import AddTask from './components/AddTask.js'
+import Footer from './components/Footer.js'
+import About from './components/About.js'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
@@ -75,11 +78,20 @@ function App() {
   }
 
   return (
-    <div className='container'>
-      <Header title={'Task Tracker'} addButtonClick={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask} />
-      {showAddTask && <AddTask addTask={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} deleteTask={deleteTask} toggleReminder={toggleReminder} /> : 'No Tasks To Show'}
-    </div>
+    <Router>
+      <div className='container'>
+        <Header title={'Task Tracker'} addButtonClick={() => setShowAddTask(!showAddTask)} showAddTask={showAddTask} />
+        <Routes>
+          <Route path="/" exact element={
+            <>
+              {showAddTask && <AddTask addTask={addTask} />}
+              {tasks.length > 0 ? <Tasks tasks={tasks} deleteTask={deleteTask} toggleReminder={toggleReminder} /> : 'No Tasks To Show'}
+            </>} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Button from './Button.js'
+import { useLocation } from 'react-router-dom';
+
 // import PropTypes from 'prop-types'
 
 export default function Header({ title, addButtonClick, showAddTask }) {
+  const location = useLocation();
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={addButtonClick} />
+      {location.pathname === '/' && (<Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={addButtonClick} />)}
     </header>
   )
 }
